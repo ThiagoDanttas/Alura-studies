ff2d63bfbdcbfb6c77a281157abf99569fa5f98f
Thiago C Dantas <60719786+ThiagoDanttas@users.noreply.github.com>
2022-07-17 19:50:54 -0300

    segundo commit

--- Projetos/alura-studies/src/components/Formulario/index.tsx
+++ Projetos/alura-studies/src/components/Formulario/index.tsx
@@ -1,35 +1,70 @@
 import React from "react";
+import { ITarefa } from "../../types/tarefa";
 import Botao from "../Botao";
 import style from "./formulario.module.scss";
-function Formulario() {
-  return (
-    <form className={style.novaTarefa}>
-      <h2>Tarefas</h2>
-      <div className={style.inputContainer}>
-        <label htmlFor="tarefa"></label>
-        <input
-          type="text"
-          name="tarefa"
-          id="tarefa"
-          placeholder="O que você quer estudar ?"
-          required
-        />
-      </div>
-      <div className={style.inputContainer}>
-        <label htmlFor="tempo">Tempo</label>
-        <input
-          type="time"
-          name="tempo"
-          id="tempo"
-          step={1}
-          min={"00:00:00"}
-          max={"03:00:00"}
-          required
-        />
-      </div>
-      <Botao />
-    </form>
-  );
+import { v4 as uuidv4 } from "uuid";
+
+class Formulario extends React.Component<{
+  setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>;
+}> {
+  state = {
+    tarefa: "",
+    tempo: "00:00",
+  };
+
+  adcionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    this.props.setTarefas((tarefasAntigas) => [
+      ...tarefasAntigas,
+      { ...this.state, selecionado: false, completado: false, id: uuidv4() },
+    ]);
+    this.setState({
+      tarefa: "",
+      tempo: "00:00",
+    });
+  }
+
+  render() {
+    return (
+      <form
+        className={style.novaTarefa}
+        onSubmit={this.adcionarTarefa.bind(this)}
+      >
+        <h2>Tarefas</h2>
+        <div className={style.inputContainer}>
+          <label htmlFor="tarefa"></label>
+          <input
+            type="text"
+            name="tarefa"
+            value={this.state.tarefa}
+            onChange={(evento) =>
+              this.setState({ ...this.state, tarefa: evento.target.value })
+            }
+            id="tarefa"
+            placeholder="O que você quer estudar ?"
+            required
+          />
+        </div>
+        <div className={style.inputContainer}>
+          <label htmlFor="tempo">Tempo</label>
+          <input
+            type="time"
+            name="tempo"
+            id="tempo"
+            value={this.state.tempo}
+            onChange={(evento) =>
+              this.setState({ ...this.state, tempo: evento.target.value })
+            }
+            step={1}
+            min={"00:00:00"}
+            max={"03:00:00"}
+            required
+          />
+        </div>
+        <Botao type="submit">Adicionar</Botao>
+      </form>
+    );
+  }
 }
 
 export default Formulario;
---
+++ Projetos/alura-studies/src/components/Lista/item/index.tsx
@@ -0,0 +1,14 @@
+import { ITarefa } from "../../../types/tarefa";
+import style from "../Lista.module.scss";
+
+function Item({ tarefa, tempo, selecionado, completado, id }: ITarefa) {
+  console.log("item atual: ", { tarefa, tempo, selecionado, completado, id });
+  return (
+    <li className={style.item}>
+      <h3>{tarefa}</h3>
+      <span>{tempo}</span>
+    </li>
+  );
+}
+
+export default Item;
